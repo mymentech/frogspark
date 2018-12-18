16f76d9bae32a5e60aaef940c9c076e8945c26bc
Mozammel Haque <mzmlhaque61@gmail.com>
2018-12-19 00:34:14 +0600

    Added placehoder section of quiz page

---
+++ page-templates/quiz.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Quiz Page
+ */
+
+get_header();
+//Loading Menu
+get_template_part('navigation-default');
+?>
+
+
+<section id="quiz_main_section" class="relative__item content__section">
+    <div class="container">
+      <div class="row">
+          <div class="col-md-6 offset-md-3">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias debitis dolores eligendi impedit nihil nostrum rem? Aliquam beatae dolores eligendi eveniet facilis quae, recusandae tempore vitae! Consectetur, eum iure.</p>
+          </div>
+      </div>
+    </div>
+  </section>
