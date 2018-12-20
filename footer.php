45e169d858b194dfbc33f0cbbfda046212909d4c
MymenTech <mymentech@gmail.com>
2018-12-20 20:26:08 +0600

    Included social profile link in footer

--- footer.php
+++ footer.php
@@ -1,99 +1,109 @@
-
 <!-- nourished-vitamin-area  start-->
-  
 
-  <div id="nourished-vitamin-area" class="text-center">
+
+<div id="nourished-vitamin-area" class="text-center">
     <h3 class="fheading">the power of seven: the possibility of a billion</h3>
     <section id="nourshed-vitamins">
-      <div class="container">
-        <h1>Nourish<span class="txt-rotate" >E</span>d vitamins</h1>
-        <h4>The clever way to get nourish<span class="txt-rotate" >E</span>d</h4> 
-        <div class="row" id="vitamin-list">
-        
-            
-              <div class="single-items">
-                  <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/vitamin-icon.png" alt="">
-                  <h2>Fresh</h2>
-                  <p>Made to order conveniently delivered every onth</p>
-              </div>
-          
-              <div class="single-items">
-              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/vitamin-icon.png" alt="">
-                  <h2>patented</h2>
-                  <p>Three Propeietary patented technologies</p>
-              </div>
+        <div class="container">
+            <h1>Nourish<span class="txt-rotate">E</span>d vitamins</h1>
+            <h4>The clever way to get nourish<span class="txt-rotate">E</span>d</h4>
+            <div class="row" id="vitamin-list">
 
-              <div class="single-items">
-              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/vitamin-icon.png" alt="">
-                  <h2>encapsulation</h2>
-                  <p>Tasty and chewable, optimum absorption</p>
-              </div>  
-          
-              <div class="single-items">
-              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/vitamin-icon.png" alt="">
-                  <h2>3d-printed</h2>
-                  <p>Seven perfectly balanced and proportioned layers</p>
-              </div>
 
-              <div class="single-items">
-              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/vitamin-icon.png" alt="">
-                  <h2>quality</h2>
-                  <p>Vegan ingredients, natural fruit fibre sweeteners</p>
-              </div>
-            
-  
-      </div>
-    </div>
+                <div class="single-items">
+                    <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/vitamin-icon.png"
+                         alt="">
+                    <h2>Fresh</h2>
+                    <p>Made to order conveniently delivered every onth</p>
+                </div>
+
+                <div class="single-items">
+                    <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/vitamin-icon.png"
+                         alt="">
+                    <h2>patented</h2>
+                    <p>Three Propeietary patented technologies</p>
+                </div>
+
+                <div class="single-items">
+                    <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/vitamin-icon.png"
+                         alt="">
+                    <h2>encapsulation</h2>
+                    <p>Tasty and chewable, optimum absorption</p>
+                </div>
+
+                <div class="single-items">
+                    <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/vitamin-icon.png"
+                         alt="">
+                    <h2>3d-printed</h2>
+                    <p>Seven perfectly balanced and proportioned layers</p>
+                </div>
+
+                <div class="single-items">
+                    <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/vitamin-icon.png"
+                         alt="">
+                    <h2>quality</h2>
+                    <p>Vegan ingredients, natural fruit fibre sweeteners</p>
+                </div>
+
+
+            </div>
+        </div>
 
     </section>
-  </div>
+</div>
 
-    <div class="check-faq">
-              <p>got a question? check our faqs</p>
-              <a href="#" class="btn">View faqs</a>
+<div class="check-faq">
+    <p>got a question? check our faqs</p>
+    <a href="#" class="btn">View faqs</a>
+</div>
+
+
+<!-- Footer Start-->
+
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer-logo">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/footer-logo.png" alt="logo">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <?php
+                wp_nav_menu(array(
+                    'menu'            => 'footer-menu',
+                    'container_class' => 'footer-pages text-center',
+                    'menu_class'      => 'list-unstled list-inline',
+                ));
+                ?>
+                <p class="copy-right-text text-center">These statements have not been evaluted by the Food and Drug
+                    Administration. This product is not intended to diagnose, treat, cure or prevent any disease.</p>
+            </div>
+            <div class="col-md-2">
+                <div class="social-icons">
+                    <ul class="list-unstyled list-inline">
+                        <li><a href="//www.instagram.com/iamnourish3d/" target="_blank"><img
+                                        src="<?php echo get_template_directory_uri() ?>/img/instagram-icon.png"
+                                        alt="instagram"></a></li>
+                        <li><a href="//twitter.com/IamNourish3d" target="_blank"><img
+                                        src="<?php echo get_template_directory_uri() ?>/img/twitter-icon.png"
+                                        alt="twitter"></a></li>
+                        <li><a href="//www.facebook.com/Nourish3d-578632799204607/" target="_blank"><img
+                                        src="<?php echo get_template_directory_uri() ?>/img/facebook-icon.png"
+                                        alt="facebook"></a></li>
+                    </ul>
+                    <small>Website by Artisan</small>
+                </div>
+            </div>
+        </div>
+
     </div>
+</footer>
 
-
-    
-   <!-- Footer Start-->
-
-   <footer>
-     <div class="container-fluid">
-       <div class="row">
-         <div class="col-md-12">
-         <div class="footer-logo">
-           <img src="<?php echo get_template_directory_uri() ?>/img/footer-logo.png" alt="logo" >
-         </div>
-         </div>
-       </div>
-       <div class="row">
-        <div class="col-md-2"></div>
-         <div class="col-md-8">
-             <?php
-             wp_nav_menu(array(
-                 'menu' => 'footer-menu',
-                 'container_class' => 'footer-pages text-center',
-                 'menu_class' => 'list-unstled list-inline',
-             ));
-             ?>
-            <p class="copy-right-text text-center">These statements have not been evaluted by the Food and Drug Administration. This product is not intended to diagnose, treat, cure or prevent any disease.</p>
-          </div>
-        <div class="col-md-2">
-            <div class="social-icons">
-            <ul class="list-unstyled list-inline">
-                <li><a href="#" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/instagram-icon.png" alt="instagram"></a> </li>
-                <li><a href="#" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/twitter-icon.png" alt="twitter"></a> </li>
-                <li><a href="#" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/facebook-icon.png" alt="facebook"></a> </li>
-            </ul>
-            <small>Website by Artisan</small>
-          </div>
-         </div>
-       </div>
-      
-     </div>
-   </footer>
-
-  <!-- nourished-vitamin-area  end-->
+<!-- nourished-vitamin-area  end-->
 
 <!-- <footer class="grey__bg">
   <div class="container">
@@ -131,96 +141,98 @@
 </footer> -->
 <?php wp_footer(); ?>
 
-<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+<link rel="stylesheet" type="text/css"
+      href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css"/>
 <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
 <script src="/app/themes/frogspark/js/dist/mp-form.js"></script>
 <script src="https://use.fontawesome.com/f4ad562077.js"></script>
 <script>
-window.addEventListener("load", function(){
-window.cookieconsent.initialise({
-  "palette": {
-    "popup": {
-      "background": "#1c4a6e"
-    },
-    "button": {
-      "background": "#ffffff",
-      "text": "#1c4a6e"
+    window.addEventListener("load", function () {
+        window.cookieconsent.initialise({
+            "palette": {
+                "popup": {
+                    "background": "#1c4a6e"
+                },
+                "button": {
+                    "background": "#ffffff",
+                    "text": "#1c4a6e"
+                }
+            },
+            "position": "bottom-left",
+            "content": {
+                "href": "http://vitamyne.frogspark.agency/cookie-policy/"
+            }
+        })
+    });
+</script>
+
+
+<script>
+    var catcount = document.getElementsByClassName("accordion-cat");
+    var i;
+
+    for (i = 0; i < catcount.length; i++) {
+        catcount[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
     }
-  },
-  "position": "bottom-left",
-  "content": {
-    "href": "http://vitamyne.frogspark.agency/cookie-policy/"
-  }
-})});
 </script>
 
 
 <script>
-var catcount = document.getElementsByClassName("accordion-cat");
-var i;
 
-for (i = 0; i < catcount.length; i++) {
-  catcount[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
-</script>
+    $(function () {
+
+        $(".vitawheel__item").on({
+            mouseenter: function () {
+                //stuff to do on mouse enter
+                $(this).click()
+            },
+            mouseleave: function () {
+                //stuff to do on mouse leave
+            }
+        });
 
 
-<script>
-  
-  $(function() {
-    
-    $(".vitawheel__item").on({
-      mouseenter: function () {
-        //stuff to do on mouse enter
-        $(this).click()
-      },
-      mouseleave: function () {
-        //stuff to do on mouse leave
-      }
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 80
+                    }, 1000);
+
+                    $("#navbar").removeClass("is-active");
+                    $(".hamburger__wrapper").removeClass("is-active");
+                    $("html").removeClass("sticky-nav-html");
+                    $("body").removeClass("body-active-nav");
+
+                    return false;
+                }
+            }
+        });
     });
-    
-    
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top - 80
-          }, 1000);
-          
-          $("#navbar").removeClass("is-active");
-          $(".hamburger__wrapper").removeClass("is-active");
-          $("html").removeClass("sticky-nav-html");
-          $("body").removeClass("body-active-nav");
-          
-          return false;
-        }
-      }
-    });
-  });
-    
+
 </script>
 <script type="text/javascript">
     $(".accordion li h5").click(function () {
-      var current_li = $(this).parent();
-      $(".accordion li div").each(function(i,el) {      
-        if($(el).parent().is(current_li)) {       
-          $(el).prev().toggleClass("plus");
-          $(el).slideToggle();        
-        } else{
-          $(el).prev().removeClass("plus");
-          $(el).slideUp();
-        }
-      });
+        var current_li = $(this).parent();
+        $(".accordion li div").each(function (i, el) {
+            if ($(el).parent().is(current_li)) {
+                $(el).prev().toggleClass("plus");
+                $(el).slideToggle();
+            } else {
+                $(el).prev().removeClass("plus");
+                $(el).slideUp();
+            }
+        });
     });
     $('.accordion li > div').hide();
     $('.accordion li h5').first().addClass("plus");
