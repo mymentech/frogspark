e1e9c25f748eac35c493e3a85b4eb5c2151b3535
khairul Islam <khairuljf@gmail.com>
2018-12-20 22:47:52 +0600

    Matha nosto

--- page-templates/template-blog.php
+++ page-templates/template-blog.php
@@ -60,7 +60,7 @@ $team_members = get_field("team_members");
     <div class="contaner">
      <div class="col-md-12">
         <div class="banner-text">
-          <h1 >OUR NOURISHENTS</h1>
+          <h1 >NOURISH<span class="txt-rotate" >E</span>D HUB</h1>
         </div>
      </div>
     </div>
@@ -69,7 +69,7 @@ $team_members = get_field("team_members");
 <!-- Page banner End -->
 
 
-<section id="brain-health-content">
+<!-- <section id="brain-health-content">
 
 <div class="container">
   <div class="row">
@@ -166,15 +166,151 @@ $team_members = get_field("team_members");
   </div>
 </div>
 
-</section>
+</section> -->
+
+ <section id="blog">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="lastest-feature">
+                  <div class="featre-img">
+                      <img src="<?php echo get_template_directory_uri(); ?>/img/blog-feature.jpg" class="img-responsive" alt="">
+                  </div>
+                  <div class="post-content">
+                    <h2 class="text-center">Title of the post can go here. title of the post here</h2>
+                 
+                      <div class="row">
+                        <div class="col-md-5"><span>By Melissa Snover - Dec 03, 2018</span></div>
+                        <div class="col-md-3"><a class="feture-readmore text-center" href="#" >Read more</a></div>
+                        <div class="col-md-4"><span class="text-right pull-right">Fearure</span></div>
+                      </div>
+               
+                  </div>
+                </div>
+            </div>
+              <div class="col-md-3">
+                  <div class="filter-options">
+                    <h3>FILTERS POSTS</h3>
+
+                    <ul class="list-unstyled">
+                      <li>
+                        <a href="#">Popular</a>
+                      </li>
+                      <li>
+                          <a class="#" href="#">Latest News</a>
+                      </li>
+                      <li>
+                          <a href="#">Featured</a>
+                      </li>
+                      <li>
+                          <a href="#">Promotions</a>
+                      </li>
+                    </ul>
+
+                    <div class="blog-search">
+                      <form>
+                        <input type="text" class="form-control search" placeholder="Search">
+                      </form>
+                    </div>
+                    <div class="tag-list">
+                      <ul>
+                        <li>
+                          <a href="#"><span>></span>Tags</a>
+                        </li>
+                        <li>
+                          <a href="#"><span>></span>Archives</a>
+                        </li>
+                        <li>
+                          <a href="#"><span>></span>Categories</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="subscription-form-sec">
+                      <form>
+                        <h3>Never miss a thing! <br> Join our Newsletter...</h3>
+                        <input type="text" placeholder="Your email address" class="form-control"> 
+                        <input type="submit" class="submit" value="Submit">
+                      </form>
+                    </div>
+                  </div>
+              </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+              <div class="single-post-item">
+                <div class="post-img">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/blog-image.jpg" class="img-responsive" alt="">
+                </div>
+                <div class="single-post-content">
+                  <h3>Title will be there</h3>
+                  <a class="feture-readmore" href="#">Read more</a>
+                   <p class="text-center">by user name - date - tag </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+                <div class="single-post-item">
+                  <div class="post-img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/blog-image.jpg" class="img-responsive" alt="">
+                  </div>
+                  <div class="single-post-content">
+                     <h3>Title will be there</h3>
+                    <a class="feture-readmore" href="#">Read more</a>
+                     <p class="text-center">by user name - date - tag </p>
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                  <div class="single-post-item">
+                    <div class="post-img">
+                      <img src="<?php echo get_template_directory_uri(); ?>/img/blog-image.jpg" class="img-responsive" alt="">
+                    </div>
+                    <div class="single-post-content">
+                       <h3>Title will be there</h3>
+                      <a class="feture-readmore" href="#">Read more</a>
+                       <p class="text-center">by user name - date - tag </p>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4">
+                    <div class="single-post-item">
+                      <div class="post-img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/blog-image.jpg" class="img-responsive" alt="">
+                      </div>
+                      <div class="single-post-content">
+                         <h3>Title will be there</h3>
+                        <a class="feture-readmore text-center" href="#">Read more</a>
+                        <p class="text-center">by user name - date - tag </p>
+                      </div>
+                    </div>
+            </div>
+            <div class="col-md-8">
+                <div class="last-feature">
+                  <div class="featre-img">
+                      <img src="<?php echo get_template_directory_uri(); ?>/img/blog-feature.jpg" class="img-responsive" alt="">
+                  </div>
+                  <div class="post-content">
+                    <h2 class="text-center">Title of the </h2>
+                    
+                     <div class="row">
+                        <div class="col-md-5"><span>By Melissa Snover - Dec 03, 2018</span></div>
+                        <div class="col-md-3"><a class="feture-readmore text-center" href="#" >Read more</a></div>
+                        <div class="col-md-4"><span class="text-right pull-right">Fearure</span></div>
+                      </div>
+
+                  </div>
+                </div>
+            </div>
 
 
+            <a class="btn blog-load-more btn-secondary btn-info" href="#">Load More Post</a>
+            
+        </div>
 
-<div class="container">
-  
+      </div>
+    </section>
 
 
-</div>
 
 
 
