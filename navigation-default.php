5e3f053e05706152c0c56b39c42b10c5d75bdf76
Mozammel Haque <mzmlhaque61@gmail.com>
2018-12-21 15:54:18 +0600

    Fixed header navigation

--- navigation-default.php
+++ navigation-default.php
@@ -27,12 +27,11 @@
         </div>
         <div id="navbar">
           <ul class="nav navbar-nav">
-            <li data-aos="fade-down" data-aos-delay="200"><a class="sm underline__item" href="/#how-nourish-works__section">How it works</a></li>
-            <li data-aos="fade-down" data-aos-delay="300"><a class="sm underline__item" href="/#meet-nourish__section">Nourishments</a></li>
-            <li data-aos="fade-down" data-aos-delay="400"><a class="sm underline__item" href="/#n-pricing">Pricing</a></li>
-            <li data-aos="fade-down" data-aos-delay="200"><a class="sm underline__item" href="/#contact-cta__section_faq">FAQ</a></li>
-            <li data-aos="fade-down" data-aos-delay="400"><a class="sm underline__item" href="/#contact-cta__section">Contact</a></li>
-            <li data-aos="fade-down" data-aos-delay="100"><a class="sm underline__item" href="/about-us/">About Us</a></li>
+            <li data-aos="fade-down" data-aos-delay="200"><a class="sm underline__item" href="/#nourshed-vitamins">Nourish ed Vitamins</a></li>
+            <li data-aos="fade-down" data-aos-delay="300"><a class="sm underline__item" href="/#get-nourished">Get Nourished</a></li>
+            <li data-aos="fade-down" data-aos-delay="400"><a class="sm underline__item" href="/our-world/">Our world </a></li>
+              <li data-aos="fade-down" data-aos-delay="100"><a class="sm underline__item" href="/about-us/">About Us</a></li>
+              <li data-aos="fade-down" data-aos-delay="400"><a class="sm underline__item" href="/contact-us/">Contact Us</a></li>
             <li data-aos="fade-down" data-aos-delay="200"><a class="sm underline__item" href="/search/">
               Search <i class="fa fa-search" aria-hidden="true" style="padding-left: 2px;"></i></a></li>
             <li data-aos="fade-down" data-aos-delay="400"><a href="/#usp__section" class="sm btn white__bg green green__border nav__btn">Get Started</a></li>
